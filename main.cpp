bb138e226dfe05ba9dbbc0bd671ab6b2778a7a0b
valy-cu-i-grec <alexandruvalentinleonte@gmail.com>
2024-12-02 17:45:17 +0200

    matrici garduri+rotate
    de terminat rotate si snap to corner

--- main.cpp
+++ main.cpp
@@ -11,35 +11,73 @@ o-oi
 p-porci
 v-vaci
 g-gard
+q-colt/capat gard
 #-spatiu gol
 a-troaca
+
 nivel1+nivel2:dificultate start
 nivel3+nivel4:dificultate junior
 nivel5:dificultate master
-*/
 
-void initializare_Nivel(char tarc[7][9],int nivel)
+======
+CONDITII GARD
+g intalneste g = not ok
+orice alta combinatie = ok
+======
+
+*/
+char matL[5][5]= {{'q','g','q',0,0},{'g',0,0,0,0},{'g',0,0,0,0},{'g',0,0,0,0},{'q',0,0,0,'v'}};
+char matGS[5][5]= {{'q',0,0,0,0},{'g',0,0,0,0},{'q',0,0,0,0},{0,0,0,0,0},{0,0,0,0,'v'}};
+char matGL[5][5]= {{'q',0,0,0,0},{'g',0,0,0,0},{'g',0,0,0,0},{'g',0,0,0,0},{'q',0,0,0,'v'}};
+
+void roteste(char m[5][5])
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++)
+            swap(mat[i][j], mat[j][i]);
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n/2; j++)
+        {
+            swap(mat[i][j], mat[i][n-j-1]);
+        }
+    }
+}
+void initializare_Nivel(char tarc[7][9])
 {
     char buffer;
     FILE *ftarc;
+    int nivel;
+
+select_nivel:
+    cout<<"selectaza nivelul: ";
+    cin>>nivel;
     switch(nivel)
     {
     case 1:
-        ftarc=fopen("Z:\\CodeBlocks\\Projects\\IP\\Smart Farmer\\Nivele\\nivel1.txt","r+");
+        ftarc=fopen("C:\\Users\\Valy\\OneDrive\\Desktop\\IP\\SmartFarmer\\Smart-Farmer\\Smart-Farmer\\Nivele\\nivel1.txt","r+");
         break;
     case 2:
-        ftarc=fopen("Z:\\CodeBlocks\\Projects\\IP\\Smart Farmer\\Nivele\\nivel2.txt","r+");
+        ftarc=fopen("C:\\Users\\Valy\\OneDrive\\Desktop\\IP\\SmartFarmer\\Smart-Farmer\\Smart-Farmer\\Nivele\\nivel2.txt","r+");
         break;
     case 3:
-        ftarc=fopen("Z:\\CodeBlocks\\Projects\\IP\\Smart Farmer\\Nivele\\nivel3.txt","r+");
+        ftarc=fopen("C:\\Users\\Valy\\OneDrive\\Desktop\\IP\\SmartFarmer\\Smart-Farmer\\Smart-Farmer\\Nivele\\nivel3.txt","r+");
         break;
     case 4:
-        ftarc=fopen("Z:\\CodeBlocks\\Projects\\IP\\Smart Farmer\\Nivele\\nivel4.txt","r+");
+        ftarc=fopen("C:\\Users\\Valy\\OneDrive\\Desktop\\IP\\SmartFarmer\\Smart-Farmer\\Smart-Farmer\\Nivele\\nivel4.txt","r+");
         break;
     case 5:
-        ftarc=fopen("Z:\\CodeBlocks\\Projects\\IP\\Smart Farmer\\Nivele\\nivel5.txt","r+");
+        ftarc=fopen("C:\\Users\\Valy\\OneDrive\\Desktop\\IP\\SmartFarmer\\Smart-Farmer\\Smart-Farmer\\Nivele\\nivel5.txt","r+");
+        break;
+    default:
+        cout<<"nivel invalid\n";
+        goto select_nivel;
         break;
     }
+    system("CLS");
 
     for (int i=0; i<7; i++)
     {
@@ -59,9 +97,6 @@ int main()
 {
     char tarc[7][9];
     bool playing;
-    int nivel;
-    cin>>nivel;
-    initializare_Nivel(tarc,nivel);
-
+    initializare_Nivel(tarc);
     return 0;
 }
